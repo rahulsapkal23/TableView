e7b215c329d1cfabca06894fd333ae437fe25851
webwerks1 <webwerks1@Rahul-Mac-mini.local>
2016-11-18 17:15:53 +0530

    all done only back button is shifted

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,7 +4,7 @@ var items = [{
     },
     "image": {
         image: "http://www.showbizbites.com/wp-content/uploads/2013/07/Aamir-Khan-Showbiz-Bites.jpg",
-        onclick: "https://en.wikipedia.org/wiki/Ranbir_Kapoor"
+        url: "https://en.wikipedia.org/wiki/Aamir_Khan"
     },
 
     "template": "image_title"
@@ -13,8 +13,9 @@ var items = [{
         text: "born 28 September 1982 is an Indian actor and producer"
     },
     "image": {
-        image: "http://customerkart.com/wp-content/uploads/2016/01/ranbir-kapoor-wallpapers-300x215.jpg"
+        image: "http://customerkart.com/wp-content/uploads/2016/01/ranbir-kapoor-wallpapers-300x215.jpg",
 
+        url: "https://en.wikipedia.org/wiki/Ranbir_Kapoor"
     },
 
     "template": "image_title"
@@ -23,7 +24,8 @@ var items = [{
         text: "born on 14 March 1965 is an Indian film actor, director and producer. Through his career in "
     },
     "image": {
-        image: "http://www.showbizbites.com/wp-content/uploads/2013/07/Aamir-Khan-Showbiz-Bites.jpg"
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Salmanrampwalk.png/220px-Salmanrampwalk.png",
+        url: "https://en.wikipedia.org/wiki/Salman_Khan"
     },
 
     "template": "image_title"
@@ -37,16 +39,39 @@ $.dynamicListView.sections[0].setItems(items, {
 
 //$.aamir.image = data.image[0];
 
-$.win.open();
-
-function WebView(e) {
+function openWebViewUrl(url) {
     var webview = Titanium.UI.createWebView({
-        Ti.API.info("url isssss" + e.);
-        url: items[]
+        "url": url
     });
+
+    var button = Titanium.UI.createButton({
+        title: 'Back',
+        top: 10,
+        width: 100,
+        height: 50
+    });
+    button.addEventListener('click', function(e) {
+        window.close();
+    });
+
     var window = Titanium.UI.createWindow();
     window.add(webview);
+    window.add(button);
+
     window.open({
         modal: true
     });
 }
+
+function WebView(e) {
+    var url;
+
+    if (Ti.Platform.osname == "android") {
+        url = e.section.items[e.itemIndex].image.url;
+    } else {
+        url = e.source.url;
+    }
+
+    openWebViewUrl(url);
+}
+$.win.open();
